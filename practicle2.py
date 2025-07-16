076ea17816cf57313775cb18917b2dd325b1be04
pritesh0605 <riddhi.karlekar24@mmit.edu.in>
2025-07-16 15:19:42 +0530

    practicle lab 2 assigment

---
+++ practicle2.py
@@ -0,0 +1,22 @@
+print("Student Course Suggestion System")
+print("Subjects you can choose from:")
+print("1. maths 2.physics 3.programming 3.graphics 4.circuits 5.statistics 6.ai")
+fav1 = input("Enter your favorite subject first (subject should only maths or statistics): ")
+fav2 = input("Enter your favorite subject second : ")
+if fav1 == "MATHS" and fav2 == "PHYSICS":
+   print("You can choose Engineering Science.")
+elif fav1 == "MATHS" and fav2 == "PROGRAMMING":
+   print("You can choose Computer Science.")
+elif fav1 == "MATHS" and fav2 == "GRAPHICS":
+   print("You can choose Mechanical Engineering.")
+elif fav1 == "MATHS" and fav2 == "CIRCUITS":
+   print("You can choose Mechatronics Engineering.")
+elif fav1 == "STATISTICS" and fav2 == "PROGRAMMING":
+   print("You can choose AI&DS Engineering.")
+elif fav1 == "STATISTICS" and fav2 == "AI":
+   print("You can choose AI&ML Engineering.")
+elif fav1 == "MATHS" and fav2 == "AI":
+   print("You can choose RO&AI Engineering.")
+else:
+   print("Sorry, no suggestions available for this combination.")
+
